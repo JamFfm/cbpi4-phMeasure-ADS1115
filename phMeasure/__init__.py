ebcbf71a94aaf061a5382d8429562a53edf93f11
JamFfm <Jan.Battermann@t-online.de>
2021-12-29 17:01:21 +0100

    first try to add calibration as Action

--- phMeasure/__init__.py
+++ phMeasure/__init__.py
@@ -8,6 +8,10 @@ from cbpi.controller.sensor_controller import SensorController
 from .ADS1x15 import ADS1115
 from aiohttp import web
 
+###################
+# Code NOT Tested #
+###################
+
 # Choose a gain of 1 for reading voltages from 0 to 4.09V.
 # Or pick a different gain to change the range of voltages that are read:
 #  - 2/3 = +/-6.144V
@@ -34,13 +38,47 @@ logger = logging.getLogger(__name__)
                              description="Select which type of data to register for this sensor, "
                                          "hint: add the same sensor several times with different units"),
              Property.Select("ADS1x15 Gain", options=["0", "1", "2", "4", "8", "16"],
-                             description="Select gain of pH ADS1x15, default = 1, hint 2/3 can be selected by 0")])
+                             description="Select gain of pH ADS1x15, default = 1, hint 2/3 can be selected by 0"),
+             Property.Number(label="Factor", configurable=True, description="Here you can adjust the factor from "
+                                                                            "calibration. Default is 0.17826")])
 class phSensorADS1x15(CBPiSensor):
 
     def __init__(self, cbpi, id, props):
         super(phSensorADS1x15, self).__init__(cbpi, id, props)
         self.value = 0
         self.key = self.props.get("phSensorADS1x15", None)
+
+    @action(key="Calibrate phSensor", parameters=[Property.Number(label="Volt at ph7.00", configurable=True, default_value=2.50,
+                                                                  description="Please enter the Voltage measured at "
+                                                                              "ph4.01. Offset is required before."),
+                                                  Property.Number(label="Volt at ph4.01", configurable=True, default_value=3.05,
+                                                                  description="Please enter the Voltage measured at "
+                                                                              "ph4.01. Offset is required before.")])
+    async def Calibrate(self, volt7=2.50, volt4=3.05, **kwargs):
+        # PH_step = (voltage@PH7 - voltage@PH4.01) / (PH7 - PH4.01) = (2.5-3.05) / (7-4.01) = (-.55/2.99) = -0.1839....
+        ph_step = (volt7 - volt4)/(-2.99)
+        self.next = False
+        self.ph_step = float(ph_step)
+        if self.ph_step <= 0:
+            self.cbpi.notify("phSensor Calibration Error", "Voltage for calibration must be larger than 0",
+                             NotificationType.ERROR, action=[NotificationAction("Next", self.NextStep)])
+            return
+        logging.info(self.ph_step)
+        self.calibration_active = True
+
+        logging.info("Calibrate phSensor")
+        logging.info("Volt7 {}, Volt4 {}".format(volt7, volt4))
+        logging.info(self.ph_step)
+        self.cbpi.notify("phSensor Calibration done", "Enter this value as phSensor factor: {}".format(self.ph_step)
+                         , action=[NotificationAction("Back", self.NextStep)])
+        while not self.next is True:
+            await asyncio.sleep(1)
+            pass
+        self.next = False
+
+    async def NextStep(self):
+        self.next = True
+        pass
 
     def get_unit(self):
         unit = self.props.get("Data Type")
@@ -53,17 +91,28 @@ class phSensorADS1x15(CBPiSensor):
         else:
             return "select Data Type"
 
+    def get_factor(self):
+        factor = self.props.get("Factor")
+        if factor == "":
+            return 0.17826
+        elif factor is None:
+            return 0.17826
+        else:
+            return factor
+        pass
     async def run(self):
         while self.running is True:
             ch = int(self.props.get("ADS1x15 Channel"))
             gain = int(self.props.get("ADS1x15 Gain"))
             address = int(self.props.get("ADS1x15 Address"), 16)
             adc = ADS1115(address=address, busnum=1)
+            factor = float(self.get_factor())
 
             value = adc.read_adc(ch, gain=gain)
             voltage = ((float(value) * 4.096 / 32767) - VOLTAGEOFFSET)
             # phvalue = ("%.2f" % (7 + ((2.564 - voltage) / 0.1839)))                        # better around pH 7 and 6
-            phvalue = ("%.2f" % (7 + ((2.548 - voltage) / 0.17826)))                         # better around pH 5
+            # phvalue = ("%.2f" % (7 + ((2.548 - voltage) / 0.17826)))                       # better around pH 5
+            phvalue = ("%.2f" % (7 + ((2.548 - voltage) / factor)))
 
             kind_of_data = self.props.get("Data Type")
             if kind_of_data == "pH Value":
